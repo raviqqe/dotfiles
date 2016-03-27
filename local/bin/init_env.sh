5df5e2bc771c0e502d5291f83434affb107a3d5a
Raven Iqqe <raviqqe@gmail.com>
2016-03-27 20:48:50 +0900

    Clone antigen only when it doesn't exist

--- local/bin/init_env.sh
+++ local/bin/init_env.sh
@@ -2,6 +2,11 @@
 
 go get github.com/motemen/ghq &&
 go get github.com/peco/peco/cmd/peco &&
-vim +PluginInstall +qall
+vim +PluginInstall +qall &&
 
-git clone https://github.com/zsh-users/antigen.git $HOME/.antigen
+antigen_dir=$HOME/.antigen
+
+if ! [ -d "$antigen_dir" ]
+then
+  git clone https://github.com/zsh-users/antigen.git "$antigen_dir"
+fi
