6e139ed989f2b338d0dfa3e4c45d2408be23b451
Raven Iqqe <raviqqe@gmail.com>
2016-03-31 16:06:58 +0900

    Add shell script utilities

--- local/libexec/dotfiles/util.sh
+++ local/libexec/dotfiles/util.sh
@@ -5,3 +5,27 @@ on_linux() {
 on_freebsd() {
   [ "$(uname)" = FreeBSD ]
 }
+
+is_git_repo() {
+  if [ $# -ne 1 ]
+  then
+    error "is_git_repo():" "Invalid number of arguments" >&2
+    return 1
+  fi
+
+  [ -d "$1/.git" ]
+}
+
+info() {
+  echo "$@" >&2
+}
+
+warn() {
+  info "WARNING:" "$@"
+  return 1
+}
+
+error() {
+  info "ERROR:" "$@"
+  return 1
+}
