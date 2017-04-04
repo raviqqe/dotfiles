27431b1047f83e671a914324bbd4a0cdb0eb5f0f
Yota Toyama <raviqqe@gmail.com>
2017-04-05 00:07:23 +0900

    Install git-lfs

--- local/bin/updot.sh
+++ local/bin/updot.sh
@@ -74,7 +74,7 @@ install_linuxbrew_packages() {
   brew install libevent --without-doxygen &&
   brew install zsh git tmux lynx links irssi bmake htop tig \
                ruby python python3 node npm gawk --without-icu4c &&
-  # ghc haskell-stack elm-format
+  # ghc haskell-stack elm-format git-lfs
 
   brew install neovim/neovim/neovim --without-jemalloc &&
 
