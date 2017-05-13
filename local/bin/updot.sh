d8abf623e674d404e1dbcde17012e3750ec7039f
Yota Toyama <raviqqe@gmail.com>
2017-05-13 11:37:15 +0900

    Install flake8

--- local/bin/updot.sh
+++ local/bin/updot.sh
@@ -189,7 +189,7 @@ install_go_packages() {
 install_python_packages() {
   info_installing "python packages" &&
   pip3 install --user --upgrade \
-      autopep8 awscli frosted mkdocs mypy shakyo pylama pylint \
+      autopep8 awscli flake8 frosted mkdocs mypy shakyo pylama pylint \
       twine vim-vint
 }
 
