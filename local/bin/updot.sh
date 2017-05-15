332a3fcfe48287e6d2eecf29ec572e1993c45d20
Yota Toyama <raviqqe@gmail.com>
2017-05-16 01:21:24 +0900

    Install go only on x86_64

--- local/bin/updot.sh
+++ local/bin/updot.sh
@@ -85,7 +85,7 @@ install_linuxbrew_packages() {
 
   brew install --without-icu4c --without-doxygen \
       zsh git tmux lynx links bmake htop tig \
-      go ruby python python3 node npm yarn gawk gnu-sed &&
+      $(is_x86_64 && echo go) ruby python python3 node npm yarn gawk gnu-sed &&
   # ghc haskell-stack elm-format git-lfs
 
   brew tap neovim/neovim &&
