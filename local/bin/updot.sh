3c98ec0eb5be8c8a5419d01c618e73a352d915e8
Yota Toyama <raviqqe@gmail.com>
2017-05-19 00:23:22 +0900

    Install typescript

--- local/bin/updot.sh
+++ local/bin/updot.sh
@@ -217,7 +217,7 @@ install_npm_packages() {
 
   npm install -g \
       babel-eslint diff-so-fancy gulp js-beautify jshint jsonlint git-recall \
-      mocha remark-cli serverless standard stylelint yarn
+      mocha remark-cli serverless standard stylelint tslint typescript yarn
 }
 
 install_vim_plug() {
