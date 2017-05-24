101833f2fb1da2f8dcc03902ee3233b616bc0d29
Yota Toyama <raviqqe@gmail.com>
2017-05-24 15:22:06 +0900

    Install karma

--- local/bin/updot.sh
+++ local/bin/updot.sh
@@ -216,7 +216,7 @@ install_ruby_gem_credential() {
 install_yarn_packages() {
   info_installing "yarn packages" &&
 
-  packages='babel-eslint diff-so-fancy gulp js-beautify jshint jsonlint
+  packages='babel-eslint diff-so-fancy gulp js-beautify jshint jsonlint karma
             git-recall mocha npm npm-check-updates remark-cli serverless
             standard stylelint tslint typescript typescript-formatter'
   yarn global add $packages &&
