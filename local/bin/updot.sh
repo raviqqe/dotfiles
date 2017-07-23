b11c4cf6a1518f6377f41239af50d9b8edba8c06
Yota Toyama <raviqqe@gmail.com>
2017-07-24 00:39:31 +0900

    Uninstall bmake

--- local/bin/updot.sh
+++ local/bin/updot.sh
@@ -87,7 +87,7 @@ install_linuxbrew_packages() {
   brew install \
       --without-docs --without-doxygen --without-icu4c --without-jemalloc \
       --without-libgit2 \
-      curl $(is_x86_64 && echo exa) zsh git tmux lynx links bmake htop tig \
+      curl $(is_x86_64 && echo exa) zsh git tmux lynx links htop tig \
       $(is_x86_64 && echo go) ruby python python3 neovim node nvm gawk \
       gnu-sed yarn &&
   # ghc haskell-stack elm-format git-lfs
