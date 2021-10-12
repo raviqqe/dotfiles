a53470e82f5feb978b08bbff70fc9b2eb4d2a3ff
Yota Toyama <raviqqe@gmail.com>
2021-10-12 03:05:25 +0000

    Fix nvim-cmp mapping

--- config/nvim/lua/init/nvim-cmp.lua
+++ config/nvim/lua/init/nvim-cmp.lua
@@ -1,11 +1,9 @@
-require('init.util')
-
 local cmp = require('cmp')
 
 cmp.setup {
     mapping = {
-        ['<c-p>'] = cmp.mapping.select_prev_item(),
-        ['<c-n>'] = cmp.mapping.select_next_item(),
+        ['<s-tab>'] = cmp.mapping.select_prev_item(),
+        ['<tab>'] = cmp.mapping.select_next_item(),
         ['<cr>'] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Insert,
             select = true
@@ -14,13 +12,5 @@ cmp.setup {
     sources = {{name = 'buffer'}, {name = 'nvim_lsp'}, {name = 'path'}}
 }
 
-local options = {expr = true, noremap = true, silent = true}
-
-vim.api.nvim_set_keymap('i', '<tab>', "v:lua.pumvisible('<c-n>', '<tab>')",
-                        options)
-vim.api.nvim_set_keymap('i', '<s-tab>', "v:lua.pumvisible('<c-p>', '<tab>')",
-                        options)
-
-vim.api.nvim_exec([[
-    autocmd Rc FileType qf nnoremap <buffer> <cr> <cr>:cclose<cr>
-]], true)
+vim.api.nvim_exec(
+    [[autocmd Rc FileType qf nnoremap <buffer> <cr> <cr>:cclose<cr>]], true)
--- config/nvim/lua/init/util.lua
+++
@@ -1,7 +0,0 @@
-local function special_key(key)
-    return vim.api.nvim_replace_termcodes(key, true, true, true)
-end
-
-function _G.pumvisible(one, other)
-    return vim.fn.pumvisible() == 1 and special_key(one) or special_key(other)
-end
