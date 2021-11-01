d533e0b1a76a47847eea669ad285fef1df71bd92
Yota Toyama <raviqqe@gmail.com>
2021-11-01 16:33:49 -0700

    Disable eslint for performance

--- config/nvim/lua/init/null-ls.lua
+++ config/nvim/lua/init/null-ls.lua
@@ -2,12 +2,13 @@ local null_ls = require('null-ls')
 
 null_ls.config({
     sources = {
-        null_ls.builtins.diagnostics.eslint,
+        -- null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.diagnostics.pylint,
         null_ls.builtins.diagnostics.rubocop, null_ls.builtins.diagnostics.vint,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.clang_format,
-        null_ls.builtins.formatting.eslint, null_ls.builtins.formatting.gofmt,
+        -- null_ls.builtins.formatting.eslint,
+        null_ls.builtins.formatting.gofmt,
         null_ls.builtins.formatting.goimports,
         null_ls.builtins.formatting.lua_format,
         null_ls.builtins.formatting.prettier,
