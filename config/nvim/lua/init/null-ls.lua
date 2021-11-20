e37600c3e43eb7afb396aa4005d9e14451d71321
Yota Toyama <raviqqe@gmail.com>
2021-11-20 04:56:37 +0000

    Configure rubocop formatting

--- config/nvim/lua/init/null-ls.lua
+++ config/nvim/lua/init/null-ls.lua
@@ -12,6 +12,7 @@ null_ls.config({
         null_ls.builtins.formatting.goimports,
         null_ls.builtins.formatting.lua_format,
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.rubocop,
         null_ls.builtins.formatting.rustfmt.with({ignore_stderr = true}),
         null_ls.builtins.formatting.shfmt.with({args = {"-i", "2"}}),
         null_ls.builtins.formatting.taplo,
