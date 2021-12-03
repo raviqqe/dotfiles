2fc6d5330e062c25d4c947b992e94931acc5c9fb
Yota Toyama <raviqqe@gmail.com>
2021-12-03 03:19:09 +0000

    Disable goimports

--- config/nvim/lua/init/null-ls.lua
+++ config/nvim/lua/init/null-ls.lua
@@ -9,7 +9,6 @@ null_ls.config({
         null_ls.builtins.formatting.clang_format,
         -- null_ls.builtins.formatting.eslint,
         null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.goimports,
         null_ls.builtins.formatting.lua_format,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.rubocop,
