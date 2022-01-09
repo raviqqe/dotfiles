6414572ebf48fb95c44a6c5a541207c16c6f178b
Yota Toyama <raviqqe@gmail.com>
2022-01-09 23:08:26 +0000

    Disable auto close temporarily

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -120,7 +120,8 @@ require('tmux').setup({
 })
 require('trouble').setup({
     auto_open = true,
-    auto_close = true,
+    -- TODO Enable auto close.
+    -- auto_close = true,
     fold_open = 'v',
     fold_closed = '>',
     icons = false,
