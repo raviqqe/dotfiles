12a1bed1a0e3256ff8bcea3db13dac1999aa6e82
Yota Toyama <raviqqe@gmail.com>
2022-09-28 11:37:58 -0700

    Disable eslint

--- config/nvim/lua/init/null_ls.lua
+++ config/nvim/lua/init/null_ls.lua
@@ -3,7 +3,6 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.completion.spell,
-		null_ls.builtins.diagnostics.eslint,
 		null_ls.builtins.formatting.black,
 		null_ls.builtins.formatting.clang_format,
 		null_ls.builtins.formatting.elm_format,
