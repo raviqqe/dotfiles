8217f70f67d772a01b53f23868d388c5af308384
Yota Toyama <raviqqe@gmail.com>
2025-09-16 08:58:27 -0700

    Use native grep

--- config/nvim/lua/init/fuzzy.lua
+++ config/nvim/lua/init/fuzzy.lua
@@ -4,9 +4,10 @@ vim.env.FZF_DEFAULT_OPTS = nil
 
 fzf.setup({
   defaults = { file_icons = false },
+  fzf_opts = { ['--layout'] = 'default' },
+  grep = { no_header_i = true },
   -- spell-checker: disable-next-line
   winopts = { fullscreen = true },
-  fzf_opts = { ['--layout'] = 'default' },
 })
 
 local options = { noremap = true }
@@ -14,5 +15,5 @@ local options = { noremap = true }
 vim.keymap.set("n", "<leader>b", fzf.buffers, options)
 vim.keymap.set("n", "<leader>c", fzf.command_history, options)
 vim.keymap.set("n", "<leader>f", fzf.files, options)
-vim.keymap.set("n", "<leader>g", fzf.live_grep, options)
+vim.keymap.set("n", "<leader>g", fzf.live_grep_native, options)
 vim.keymap.set("n", "<leader>r", fzf.git_files, options)
